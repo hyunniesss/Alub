8649e1c5e7ff8088c7b2d1b4bc682c232ceb0dcc
hyunniesss <59987117+hyunniesss@users.noreply.github.com>
2021-11-19 13:34:39 +0900

    BOJ-1000 'A+B' [ 21/11/19 13:34:39 ] by Alub

--- BOJ/1000/1000_1.java
+++ BOJ/1000/1000_1.java
@@ -1,22 +1,18 @@
 /**
-* 메모리: 11520 KB, 시간: 80 ms
+* 메모리: 12896 KB, 시간: 120 ms
 * 2021.11.19
 * by Alub
 */
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		int answer = 0;
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
-			StringTokenizer str = new StringTokenizer(br.readLine(), " ");
-			answer = Integer.parseInt(str.nextToken()) + Integer.parseInt(str.nextToken());
-		} catch (Exception e) {
-		}
-		System.out.println(answer);
-	}
-
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+        sc.close();
+        
+    }
+    
 }
