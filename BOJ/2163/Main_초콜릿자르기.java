522d0e66c237c812b9bcdc08818fc7e30404437f
hyunniesss <59987117+hyunniesss@users.noreply.github.com>
2021-12-03 22:02:38 +0900

    BOJ-2163 '초콜릿 자르기' [ 21/12/03 22:02:38 ] by Alub

---
+++ BOJ/2163/Main_초콜릿자르기.java
@@ -0,0 +1,18 @@
+/**
+* 메모리: 12880 KB, 시간: 108 ms
+* 2021.12.03
+* by Alub
+*/
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*sc.nextInt() - 1);
+        sc.close();
+
+    }
+
+}
