6cfba08650a23fe9789e8c4b41c00e9b39a4c806
hyunniesss <59987117+hyunniesss@users.noreply.github.com>
2021-12-04 18:28:42 +0900

    BOJ-10886 '0 = not cute / 1 = cute' [ 21/12/04 18:28:42 ] by Alub

---
+++ BOJ/10886/Main_0 = not cute / 1 = cute.java
@@ -0,0 +1,33 @@
+/**
+* 메모리: 11476 KB, 시간: 76 ms
+* 2021.12.04
+* by Alub
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+public static void main(String[] args) {
+    // 입력 - 한 사람의 수 N (1 ≤ N ≤ 101, N은 홀수)
+    // 0 : Junhee is not cute!, 1 : Junhee is cute!
+
+    solve();
+}
+
+private static void solve(){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    try{
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[2];
+        while(N-- >0){
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        System.out.println(arr[0] > arr[1] ? "Junhee is not cute!" : "Junhee is cute!");
+    } catch(Exception e){
+        
+    }
+}
+
+}
