0da6107ff2dd6dedb099ef6eeb397478d8180f4f
hyunniesss <59987117+hyunniesss@users.noreply.github.com>
2021-12-13 09:53:55 +0900

    BOJ-11057 '오르막 수' [ 21/12/13 09:53:55 ] by Alub

---
+++ BOJ/11057/Main_오르막 수.java
@@ -0,0 +1,40 @@
+/**
+* 메모리: 11636 KB, 시간: 76 ms
+* 풀이 시간: 00:30:00
+* 2021.12.13
+* by Alub
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	
+	static final int MOD = 10007;
+	
+	public static void main(String[] args) {
+		int N = 0;
+		// 입력
+		try {
+			N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		} catch(IOException | NumberFormatException e) {}
+		
+		// 로직
+		int[][] dp = new int[N+1][10];	//n자리의 0~9까지 끝나는 수의 개수
+		Arrays.fill(dp[1], 1);
+		for(int n=2;n<=N;n++) {
+			for(int i=0;i<10;i++) {
+				for(int j=0;j<=i;j++) {
+					dp[n][i] = (dp[n][i]+dp[n-1][j])%MOD;
+				}
+			}
+		}
+		int answer = 0;
+		for(int i : dp[N]) {
+			answer = (answer+i)%MOD;
+		}
+		System.out.println(answer);
+	}
+
+}
